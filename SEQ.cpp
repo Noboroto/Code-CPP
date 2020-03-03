5f36714448e4032ee667479b65b4e072b0b702e2
David0403 <thanhtuhp+Noboroto@users.noreply.github.com>
2020-03-03 14:51:36 +0700

    Add SEQ

---
+++ SEQ.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int l, r, n;
+vector <int> Sum;
+
+int main (){
+    freopen ("SEQ.INP", "r", stdin);
+    freopen ("SEQ.OUT", "w", stdout);
+    cin >> n >> l >> r;
+    Sum.push_back(0);
+    for (int i = 1; i <= n; ++i){
+        int x;
+        cin >> x;
+        Sum.push_back (Sum[i - 1] + x);
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; ++i){
+        for (int j = i; j <= n; ++j){
+            int tmp = abs(Sum[j] - Sum[i - 1]);
+            if (l <= tmp && tmp <= r) ans++;
+        }
+    }
+    cout << ans;
+    return 0;
+}
