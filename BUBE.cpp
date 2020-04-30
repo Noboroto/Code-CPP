a1dd2bad5e6cd9d275156f70a24afa3156c69fe8
David0403 (HP Laptop) <thanhtuhp+Noboroto@users.noreply.github.com>
2020-04-30 18:59:44 +0700

    Update

--- BUBE.cpp
+++ BUBE.cpp
@@ -19,22 +19,18 @@ vector <int> a;
 
 int FindUpper (int l, int r, int x)
 {
-    int ans = -1;
+    int ans = 1e9+7;
     while (l < r)
     {
         int mid = (l + r) / 2;
-        if (mid < x) l = mid + 1;
-        if (x <= mid)
+        if (a[mid] < x) l = mid + 1;
+        if (x <= a[mid])
         {
-            if (check[mid] > 0) ans = min (ans, mid);
+            if (check[a[mid]] > 0) ans = min (ans, mid);
             r = mid - 1;
         }
     }
-    sort (a.begin(), a.end());
-    for (int i = 0; i < n; ++i)
-    {
-
-    }
+    ans = (ans == 1e9 + 7) ? -1 : ans;
     return ans;
 }
 
@@ -49,6 +45,21 @@ int main ()
         a.push_back(x);
         check[x]++;
     }
-    
+    sort (a.begin(), a.end());
+    int ans = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        int sum = a[i];
+        check[a[i]]--;
+        int tmp = FindUpper (i + 1 , n - 1, a[i] + k);
+        while (tmp != -1)
+        {
+            check[a[tmp]]--;
+            sum = a[tmp];
+            tmp = FindUpper (tmp + 1, n - 1,a[tmp] + k);
+        }
+        ans += sum;
+    }
+    cout << ans;
     return 0;
 }
