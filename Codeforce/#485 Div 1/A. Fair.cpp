e8f336539d4629871c4a9a460b9810b7bf01ff8e
David0403 <48942146+Noboroto@users.noreply.github.com>
2020-05-15 09:35:37 +0700

    Update A. Fair.cpp
    Add Input Metthod

--- Codeforce/#485 Div 1/A. Fair.cpp
+++ Codeforce/#485 Div 1/A. Fair.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
 
 using namespace std;
 
@@ -11,9 +12,29 @@ void Init ()
     freopen (FileOUT.c_str(), "w", stdout);
 }
 
+int const N = 1e5 + 1;
+int n, m, s, k, ProductOf[N];
+vector < vector <int> > Adjacency;
+
+void Input ()
+{
+    cin >> n >> m >> s >> k;
+    for (int i = 0; i < n; ++i)
+    {
+        Adjacency.push_back(vector<int>());
+        cin >> ProductOf[i];
+    }
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v;
+        cin >> u >> v;
+        Adjacency[u].push_back(v);
+        Adjacency[v].push_back(u);
+    }
+}
+
 int main ()
 {
     Init();
-    
     return 0;
 }
