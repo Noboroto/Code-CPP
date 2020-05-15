97f3f92135aeca8513629364674b022a3bdd1694
David0403 (HP Laptop) <thanhtuhp+Noboroto@users.noreply.github.com>
2020-05-15 21:47:49 +0700

    Update BNHAN2 .cpp

--- NTUCODER/BNHAN2 .cpp
+++ NTUCODER/BNHAN2 .cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,13 +12,7 @@ void Init ()
 int const N = 1e5 + 7;
 long long a[N], k, m, n;
 
-long long Check (int last)
-{
-    
-    return true;
-}
-
-long long Binary_search (long long first, long long last, long long)
+long long Binary_search ()
 {
     
 }
@@ -25,17 +21,5 @@ int main ()
 {
     Init();
     cin >> m >> n >> k;
-    int count = 0;
-    for (int i = 1; i <= n; ++i)
-    {
-        for (int j = 1; j <= m; ++j)
-        {
-            a[count] = i*j;
-            count++;
-            if (count >= N) break;
-        }
-    }
-    sort (a, a + min (count , N));
-    cout << a[k - 1];
     return 0;
 }
