ef11d073c8da30fc3adda40dde284453a58eae39
David0403 <48942146+Noboroto@users.noreply.github.com>
2020-05-25 22:13:13 +0700

    Update Launch

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+void Init ()
+{
+}
+
+int main ()
+{
+    Init();
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
