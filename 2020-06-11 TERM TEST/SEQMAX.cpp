66e07bb1830a4baa300204295e555b115158f7ae
David0403 (laptop) <48942146+Noboroto@users.noreply.github.com>
2020-06-11 07:37:35 +0700

    Update SEQMAX.cpp

--- 2020-06-11 TERM TEST/SEQMAX.cpp
+++ 2020-06-11 TERM TEST/SEQMAX.cpp
@@ -11,9 +11,32 @@ void Init ()
     freopen (FileOUT.c_str(), "w", stdout);
 }
 
+const int N = 1e4 + 1;
+int n, a[N];
+
+int Solve ()
+{
+    int j, tmp = 0, ans = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        tmp = 0;
+        for (j = i; j < n; ++j)
+        {
+            tmp += a[j];
+            if (tmp > 0) ans = max (ans, j - i + 1);
+        }
+    }
+    return ans;
+}   
+
 int main ()
 {
     Init();
-    
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+    cout << Solve();
     return 0;
 }
