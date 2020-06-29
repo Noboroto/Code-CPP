e4fe59855759856d4dc5e50e9b5f1c722a6c73a5
David0403 (laptop) <48942146+Noboroto@users.noreply.github.com>
2020-06-29 19:47:39 +0700

    Update ANT.cpp

--- 2020-06-24 TraiHeHungVuong2019/ANT.cpp
+++ 2020-06-24 TraiHeHungVuong2019/ANT.cpp
@@ -10,31 +10,57 @@ void Init ()
     freopen (FileOUT.c_str(), "w", stdout);
 }
 
-const int oo = 1e9 + 7;
-const int Dx [3] = {1, 0, 1};
-const int Dy [3] = {0, 1, 1};
 int n, m;
-long long ans = 0;
+long long const oo = 1e9 + 7;
+vector <long long> Giaithua;
 
-void DFS (int x, int y)
+void GetData ()
 {
-    if (x == n && y == m)
+    scanf ("%d %d", &n, &m);
+    if (m < m) swap (n, m);
+    long long tmp = 1;
+    Giaithua.push_back(0);
+    for (long long i = 1; i <= m + n; ++i)
     {
-        ans++;
-        ans %= oo;
-    }
-    if (x > n || y > m) return;
-    for (int i = 0; i < 3; ++i)
-    {
-        DFS (x + Dx[i], y + Dy[i]);
-    }
+        tmp = (tmp % oo * i % oo) % oo;
+        Giaithua.push_back (tmp % oo);
+    } 
+}
+
+long long Pow (long long a, long long n)
+{
+    if (n == 0) return 1;
+    long long t = Pow (a, n / 2) % oo;
+    t = (t * t) % oo;
+    if (n % 2 == 0) return t;
+    else return (t * a % oo) % oo;
+}
+
+long long ToHop (long long k, long long n)
+{
+    if (n == 0 || k == 0 || n == k) return 1;
+    if (k == 1) return n;
+    long long tu = Giaithua[n] % oo;
+    long long mau = Pow (Giaithua[k] % oo * Giaithua[n - k] % oo, oo - 2);
+    return ((tu * mau) % oo);
 }
 
 int main ()
 {
     Init();
-    scanf ("%d %d", &n, &m);
-    DFS (0, 0);
-    printf ("%lld", ans);
+    GetData();
+    if (m == 0 || n == 0)
+    {
+        cout << 1;
+        return 0;
+    }
+    long long ans = 0, a, b;
+    for (long long i = 0; i <= n; ++i)
+    {
+        a = (ToHop (i, m + n - i) % oo);
+        b = (ToHop (n - i, n + m - 2 * i) % oo);
+        ans += (a * b) % oo;
+    }
+    cout << ans;
     return 0;
 }
