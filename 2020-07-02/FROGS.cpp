bf3d7c888d9699e47f5ac4dc1f1e528f74f8e255
David0403 (laptop) <48942146+Noboroto@users.noreply.github.com>
2020-07-02 13:57:31 +0700

    Update FROGS.cpp

--- 2020-07-02/FROGS.cpp
+++ 2020-07-02/FROGS.cpp
@@ -1,55 +1,34 @@
-#include <bits/stdc++.H>
+#include <cstdio>
 
-using namespace std;
-
-void Init ()
-{
-    const string FileINP = "FROGS" + (string)".INP";
-    const string FileOUT = "FROGS" + (string)".OUT";
-    freopen (FileINP.c_str(), "r", stdin);
-    freopen (FileOUT.c_str(), "w", stdout);
-}
-
-const int oo = 1e9 + 1;
 const int N = 1e6 + 1;
 int n, H[N], J[N];
-
-void GetData ()
-{
-    scanf ("%d", &n);
-    for (int i = 1; i <= n; ++i)
-    {
-        scanf ("%d", &H[i]);
-    }
-    for (int i = 1; i <= n; ++i)
-    {
-        scanf ("%d", &J[i]);
-    }
-}
-
-void Bruce_Force_1e4 ()
-{
-    for (int i = 1; i <= n; ++i)
-    {
-        for (int j = 1; j < i; ++j)
-        {
-            if (J[j] == 0) continue;
-            if (H[i] <= H[j]) continue;
-            H[j] = H[i];
-            J[j]--;
-        }
-    }
-}
+int j, i;
 
 int main ()
 {
-    Init();
-    GetData();
-    Bruce_Force_1e4();
-    for (int i = 1; i <= n; ++i)
+    freopen ("FROGS.INP", "r", stdin);
+    freopen ("FROGS.OUT", "w", stdout);
+    scanf ("%d", &n);
+    for (i = 1; i <= n; ++i)
     {
-        if (J[i] > 0) cout << -1 << ' ';
-        else cout << H[i] << ' ';
+        scanf ("%d", &H[i]);
     }
+    for (i = 1; i <= n; ++i)
+    {
+        scanf ("%d", &J[i]);
+    }
+        for (i = 1; i <= n; ++i)
+    {
+        for (j = i + 1; j <= n && J[i] > 0; ++j)
+        {
+            if (H[i] < H[j])
+            {
+                J[i]--;
+                H[i] = H[j];
+            }
+        }
+        if (J[i] > 0) H[i] = -1;
+    }
+    for (i = 1; i <= n; ++i) printf ("%d ", H[i]);
     return 0;
 }
