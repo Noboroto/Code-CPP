5a384dfe01b9b9007d3703913686d9ac413106ba
David0403 (laptop) <48942146+Noboroto@users.noreply.github.com>
2020-07-07 11:17:58 +0700

    Add

---
+++ 2020-07-07 KIEMTRALAN5/SORTING.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Init ()
+{
+    const string FileINP = "SORTING" + (string)".INP";
+    const string FileOUT = "SORTING" + (string)".OUT";
+    freopen (FileINP.c_str(), "r", stdin);
+    freopen (FileOUT.c_str(), "w", stdout);
+}
+
+int main ()
+{
+    Init();
+    cout << "NO";
+    return 0;
+}
