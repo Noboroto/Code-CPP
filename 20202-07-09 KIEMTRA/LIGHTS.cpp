5c5cc89225b85ac642192e138bcd630b523e4070
David0403 (laptop) <48942146+Noboroto@users.noreply.github.com>
2020-07-09 09:26:14 +0700

    Create LIGHTS.cpp

---
+++ 20202-07-09 KIEMTRA/LIGHTS.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Init ()
+{
+    const string FileINP = "LIGHTS" + (string)".INP";
+    const string FileOUT = "LIGHTS" + (string)".OUT";
+    freopen (FileINP.c_str(), "r", stdin);
+    freopen (FileOUT.c_str(), "w", stdout);
+}
+
+int main ()
+{
+    Init();
+    cout << 1;
+    return 0;
+}
