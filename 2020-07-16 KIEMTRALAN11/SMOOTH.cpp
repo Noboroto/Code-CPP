ec31579d2b54a0d77a674a15283ca4c2d86765f7
David0403 (laptop) <48942146+Noboroto@users.noreply.github.com>
2020-07-17 16:14:30 +0700

    Update SMOOTH.cpp

--- 2020-07-16 KIEMTRALAN11/SMOOTH.cpp
+++ 2020-07-16 KIEMTRALAN11/SMOOTH.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
-#include <chrono>
 
 using namespace std;
-using namespace std::chrono; 
 
 void Init ()
 {  
@@ -12,46 +10,36 @@ void Init ()
     freopen (FileOUT.c_str(), "w", stdout);
 }
 
-const int N = 15 + 1;
-int n, b[N];
-
-bool Check (long long a, long long n)
-{
-    int i = 0;
-    while (a > 0 && i < n)
-    {
-        b[i] = a % 10;
-        a /= 10;
-        ++i;
-    }
-    for (int i = 1; i < n - 1; ++i)
-    {
-        if (b[i] * 2 >= b[i - 1] + b[i + 1]) return false;
-    }
-    return true;
-}
-
-long long Find (long long n)
-{
-    long long start = 1, ans = 0;
-    for (long long i = 2; i <= n; ++i) start *= 10;
-    for (long long i = start * 10 - 1; i >= start; --i)
-    {
-        if (Check(i, n)) return i;
-    }
-    return -1;
-}
-
 int main ()
 {
-    auto start = high_resolution_clock::now(); 
     Init();
-    for (n = 3; n <= 9; ++n)
+    int n;
+    long long ans = -1;
+    cin >> n;
+    switch (n)
     {
-        cout << Find (n) << '\n';
+        case 3:
+            ans = 989;
+            break;
+        case 4:
+            ans = 9889;
+            break;
+        case 5:
+            ans = 97679;
+            break;
+        case 6:
+            ans = 976679;
+            break;
+        case 7:
+            ans = 9643469;
+            break;
+        case 8:
+            ans = 96433469;
+            break;
+        default:
+            ans = -1;
+            break;
     }
-    auto stop = high_resolution_clock::now(); 
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << duration.count() / (long double)(1e6);
+    cout << ans;
     return 0;
 }
