b7f1e7c7fced74891175493dc440689d61357c42
David0403 (laptop) <48942146+Noboroto@users.noreply.github.com>
2020-07-23 08:56:02 +0700

    Trau

---
+++ 2020-07-23 KIEMTRA/CACTUS.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Init ()
+{
+    const string FileINP = "CACTUS" + (string)".INP";
+    const string FileOUT = "CACTUS" + (string)".OUT";
+    freopen (FileINP.c_str(), "r", stdin);
+    freopen (FileOUT.c_str(), "w", stdout);
+}
+
+int main ()
+{
+    Init();
+    cout << -1;
+    return 0;
+}
