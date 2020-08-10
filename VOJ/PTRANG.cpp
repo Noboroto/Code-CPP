66779b1cd52bf6f2d7ccaf57681574200296fdf7
David0403 <48942146+Noboroto@users.noreply.github.com>
2020-08-10 16:26:13 +0700

    Create PTRANG.cpp

---
+++ VOJ/PTRANG.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    https://codeforces.com/group/FLVn1Sc504/contest/274829/problem/E
+*/
+
+void Init ()
+{
+    const string FileINP = "PTRANG" + (string)".INP";
+    const string FileOUT = "PTRANG" + (string)".OUT";
+    freopen (FileINP.c_str(), "r", stdin);
+    //freopen (FileOUT.c_str(), "w", stdout);
+}
+
+int main ()
+{
+    Init();
+    
+    return 0;
+}
